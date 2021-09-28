ef3e37a7d4f9420e963ffc35ad519c6291e5dfe8
Passant Abdelgalil <basant.a.abdelgalil@gmail.com>
2021-09-28 03:03:13 +0200

    modified script file to run properly

--- week3_ws/test_scripts_part2.sh
+++ week3_ws/test_scripts_part2.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-catkin b
-gnome-terminal -- /bin/bash -c 'sleep 13; source devel/setup.bash; roslaunch assignment_sol hrwros_turtlebot_navigation.launch'
-gnome-terminal -- /bin/bash -c 'sleep 13; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=`rospack find assignment_sol`/config/map_factory_v1.yaml initial_pose_x:=-4.000538 initial_pose_y:=-0.200409'
-gnome-terminal -- /bin/bash -c 'sleep 13; source devel/setup.bash; roslaunch assignment_sol hrwros_view_navigation.launch'
-gnome-terminal -- /bin/bash -c 'sleep 13; source devel/setup.bash; roslaunch assignment_sol week3_assignment3_part2.launch; exec bash'
+catkin build
+gnome-terminal -- /bin/bash -c 'roscore'
+sleep 2
+gnome-terminal -- /bin/bash -c 'source devel/setup.bash; roslaunch assignment_sol hrwros_turtlebot_navigation.launch'
+sleep 4
+gnome-terminal -- /bin/bash -c 'source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=`rospack find assignment_sol`/config/map_factory_v1.yaml initial_pose_x:=-4.000538 initial_pose_y:=-0.200409'
+sleep 4
+gnome-terminal -- /bin/bash -c 'source devel/setup.bash; roslaunch assignment_sol hrwros_view_navigation.launch'
+sleep 4
+gnome-terminal -- /bin/bash -c 'source devel/setup.bash; roslaunch assignment_sol week3_assignment3_part2.launch; exec bash'
